71ed0a2163dab9f5e276e9829932aaf58a5dc9a9
anastasiabolotnikova <bolotnikova.anastasia@gmail.com>
2015-02-19 18:30:49 +0200

    Action Bar
    Initial functionality structure

--- app/src/main/java/com/dip/mob/mobdip/MyActivity.java
+++ app/src/main/java/com/dip/mob/mobdip/MyActivity.java
@@ -3,10 +3,13 @@ package com.dip.mob.mobdip;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
 public class MyActivity extends Activity {
+
+    private MenuItem upload, camera, ch1, ch2, ch3, ch4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,23 +17,63 @@ public class MyActivity extends Activity {
         setContentView(R.layout.activity_my);
     }
 
+    // upload picture from gallery
+    public void upload(MenuItem m){
+
+    }
+
+    // take photo and upload it to app
+    public void takePhotoAndUpload(MenuItem m){
+
+    }
+
+    // show menu for chapter 1 functionality selection
+    public void chapter1(MenuItem m){
+
+    }
+
+    public void chapter2(MenuItem m){
+
+    }
+
+    public void chapter3(MenuItem m){
+
+    }
+
+    public void chapter4(MenuItem m){
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my, menu);
+
+        upload = menu.findItem(R.id.upload);
+        camera = menu.findItem(R.id.camera);
+        ch1 = menu.findItem(R.id.ch1);
+        ch2 = menu.findItem(R.id.ch2);
+        ch3 = menu.findItem(R.id.ch3);
+        ch4 = menu.findItem(R.id.ch4);
+
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch (item.getItemId()) {
+            case R.id.upload:
+                //upload();
+                return true;
+            case R.id.camera:
+                //takePhotoAndUpload();
+                return true;
+            case R.id.ch1:
+                //chapter1();
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
