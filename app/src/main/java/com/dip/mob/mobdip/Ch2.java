176b3596326e289fc807dd34a3654a997f5c1b7a
anastasiabolotnikova <bolotnikova.anastasia@gmail.com>
2015-05-23 22:06:14 +0300

    Iiris: Ilummination chapter

--- app/src/main/java/com/dip/mob/mobdip/Ch2.java
+++ app/src/main/java/com/dip/mob/mobdip/Ch2.java
@@ -1,13 +1,21 @@
 package com.dip.mob.mobdip;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
-import com.dip.mob.mobdip.R;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 /**
  * Created by anton on 26.02.15.
@@ -17,23 +25,256 @@ import com.dip.mob.mobdip.R;
 public class Ch2 extends Fragment {
 
     private int menuID;
-    private TextView title;
+    private TextView title, seekBarLbl;
     private Bundle bundle; //need to collect data from MyActivity
+    //private View seekBarView;
+    private SeekBar seekBar;
+    private DrawerLayout layout;
+    private Action currentAction;
+    private int imgCnt;
 
-    public Ch2(){}
+    private ImageView theImage;
+    private Bitmap bitmap, resultBmp;
+    private Drawable drawable;
+    private View rootView;
+
+    private enum Action {
+        POW,ROOT, EXP, LOG, HIST, SVD
+    }
+
+
+    public Ch2() {}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState){
 
-        View rootView = inflater.inflate(R.layout.fragment_ch2, container, false);
+        layout = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+        rootView = inflater.inflate(R.layout.fragment_ch2, container, false);
         bundle = getArguments();
         menuID = bundle.getInt("childPosition");
         title = (TextView) getActivity().findViewById(R.id.textView);
-        title.setText(getResources().getStringArray(R.array.nav_drawer_kids1)[menuID]);
+        title.setText(getResources().getStringArray(R.array.nav_drawer_kids2)[menuID]);
         title = (TextView) getActivity().findViewById(R.id.textView2);
+        seekBarLbl = (TextView) getActivity().findViewById(R.id.seekBarLbl);
         title.setText("");
 
+
+        // for (int i=0;i<256;i++) //not necessary as it never gets to zero
+        //   table[0][i]=0;
+        //end of creating table
+
+
         return rootView;
+
     }
+
+    public void onResume() {
+        super.onResume();
+        seekBar = (SeekBar) getActivity().findViewById(R.id.seekBar);
+
+        theImage = (ImageView) getActivity().findViewById(R.id.image);
+
+        switch (menuID) {
+            case 0:
+                currentAction = Action.POW;
+                seekBar.setProgressDrawable(getResources().getDrawable(R.drawable.progressbar));
+                seekBar.setThumb(getResources().getDrawable(R.drawable.thumb));
+                seekBar.setMax(200);
+                seekBar.setProgress(100);
+                bitmap = ((BitmapDrawable) theImage.getDrawable()).getBitmap();
+                imgCnt = MyActivity.getInstance().getImgCnt();
+                break;
+            case 1:
+                currentAction = Action.EXP;
+                seekBar.setProgressDrawable(getResources().getDrawable(R.drawable.progressbar));
+                seekBar.setThumb(getResources().getDrawable(R.drawable.thumb));
+                seekBar.setMax(600);
+                seekBar.setProgress(272);
+                bitmap = ((BitmapDrawable) theImage.getDrawable()).getBitmap();
+                imgCnt = MyActivity.getInstance().getImgCnt();
+                break;
+            case 2:
+                currentAction = Action.LOG;
+                seekBar.setProgressDrawable(getResources().getDrawable(R.drawable.progressbar));
+                seekBar.setThumb(getResources().getDrawable(R.drawable.thumb));
+                seekBar.setMax(200);
+                seekBar.setProgress(100);
+                bitmap = ((BitmapDrawable) theImage.getDrawable()).getBitmap();
+                imgCnt = MyActivity.getInstance().getImgCnt();
+                break;
+            case 3:
+                seekBar.setProgressDrawable(getResources().getDrawable(R.drawable.progressbar));
+                seekBar.setThumb(getResources().getDrawable(R.drawable.thumb));
+                seekBar.setMax(200);
+                seekBar.setProgress(100);
+
+                currentAction = Action.HIST;
+                bitmap = ((BitmapDrawable) theImage.getDrawable()).getBitmap();
+                imgCnt = MyActivity.getInstance().getImgCnt();
+                break;
+            case 4:
+                currentAction = Action.SVD;
+
+                seekBar.setProgressDrawable(getResources().getDrawable(R.drawable.progressbar));
+                seekBar.setThumb(getResources().getDrawable(R.drawable.thumb));
+                seekBar.setMax(200);
+                seekBar.setProgress(100);
+                bitmap = ((BitmapDrawable) theImage.getDrawable()).getBitmap();
+                imgCnt = MyActivity.getInstance().getImgCnt();
+                break;
+        }
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                seekBarLbl.setText("" + progress/100.0);
+                int seekLblPos = ((((seekBar.getRight() - seekBar.getLeft()) * seekBar.getProgress()) / seekBar.getMax()) + seekBar.getLeft()) - 15;
+                seekBarLbl.setX(seekLblPos);
+                //Toast.makeText(getActivity(), "Progress: " + progress, Toast.LENGTH_SHORT).show();
+
+                switch (currentAction) {
+                    case POW:
+                        // Interpolate by the factor of progress
+                        Mat illuPOW = new Mat();
+                        Utils.bitmapToMat(bitmap, illuPOW);
+                        resultBmp = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                        Imgproc.cvtColor(illuPOW, illuPOW, Imgproc.COLOR_RGB2GRAY);
+
+                        //illuminate with progress = n
+
+                        illuPOW = power(illuPOW,progress/100.0);
+                        Utils.matToBitmap(illuPOW, resultBmp);
+                        drawable = new BitmapDrawable(resultBmp);
+                        theImage.setImageDrawable(drawable);
+                        break;
+                    case HIST:
+                        Mat illuHIST = new Mat();
+                        Utils.bitmapToMat(bitmap, illuHIST);
+                        resultBmp = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                        Imgproc.cvtColor(illuHIST, illuHIST, Imgproc.COLOR_RGB2GRAY);
+                        //illuminate with progress = n
+                        // illuHIST.convertTo(illuHIST, CvType.CV_8UC1);
+                        Imgproc.equalizeHist(illuHIST,illuHIST);
+                        Utils.matToBitmap(illuHIST, resultBmp);
+                        drawable = new BitmapDrawable(resultBmp);
+                        theImage.setImageDrawable(drawable);
+                        break;
+                    case EXP:
+                        // Interpolate by the factor of progress
+                        Mat illuEXP = new Mat();
+                        Utils.bitmapToMat(bitmap, illuEXP);
+                        resultBmp = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                        Imgproc.cvtColor(illuEXP, illuEXP, Imgproc.COLOR_RGB2GRAY);
+
+                        //illuminate with progress = n
+                        illuEXP = exp(illuEXP,progress/100.0);
+                        Utils.matToBitmap(illuEXP, resultBmp);
+                        drawable = new BitmapDrawable(resultBmp);
+                        theImage.setImageDrawable(drawable);
+                        break;
+                    case LOG:
+                        // Interpolate by the factor of progress
+                        Mat illuLOG = new Mat();
+                        Utils.bitmapToMat(bitmap, illuLOG);
+                        resultBmp = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                        Imgproc.cvtColor(illuLOG, illuLOG, Imgproc.COLOR_RGB2GRAY);
+                        illuLOG = log(illuLOG,progress/100.0);
+                        Utils.matToBitmap(illuLOG, resultBmp);
+                        drawable = new BitmapDrawable(resultBmp);
+                        theImage.setImageDrawable(drawable);
+                        break;
+                    case SVD:
+                        //do some more
+                        Mat illuSVD = new Mat();
+                        Utils.bitmapToMat(bitmap, illuSVD);
+                        resultBmp = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                        Imgproc.cvtColor(illuSVD, illuSVD, Imgproc.COLOR_RGB2GRAY);
+
+                        //illuminate with progress = n
+
+
+                        Utils.matToBitmap(illuSVD, resultBmp);
+                        drawable = new BitmapDrawable(resultBmp);
+                        theImage.setImageDrawable(drawable);
+
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                int currentImgCnt = MyActivity.getInstance().getImgCnt();
+                if (imgCnt != currentImgCnt) {
+                    bitmap = ((BitmapDrawable) theImage.getDrawable()).getBitmap();
+                    imgCnt = currentImgCnt;
+                }
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+
+    public static Mat power(Mat image, double n){
+        Mat result = new Mat(image.size(), image.type()) ;
+        int m =(int) image.elemSize();
+
+        double [] data = new double[m];
+
+        for (int i = 0; i<image.rows();i++){
+            for (int j = 0; j<image.cols();j++){
+                for(int k = 0; k<m;k++){
+                    data[k] = Math.pow(image.get(i,j)[k],n);
+                }
+                result.put(i, j, data);
+            }
+        }
+        return result;
+    }
+
+
+    public static Mat log(Mat image, double n){
+        Mat result = new Mat(image.size(), image.type()) ;
+        int m =(int) image.elemSize();
+
+        double [] data = new double[m];
+
+        for (int i = 0; i<image.rows();i++){
+            for (int j = 0; j<image.cols();j++){
+                for(int k = 0; k<m;k++){
+                    data[k] = Math.log(image.get(i,j)[k])/Math.log(n);
+                }
+                result.put(i, j, data);
+            }
+        }
+        return result;
+    }
+
+    public static Mat exp(Mat image, double n){
+        Mat result = new Mat(image.size(), image.type()) ;
+        int m =(int) image.elemSize();
+
+        double [] data = new double[m];
+
+        for (int i = 0; i<image.rows();i++){
+            for (int j = 0; j<image.cols();j++){
+                for(int k = 0; k<m;k++){
+                    data[k] = Math.pow(n,image.get(i,j)[k]);
+                }
+                result.put(i, j, data);
+            }
+        }
+        return result;
+    }
+
+
 }
+
