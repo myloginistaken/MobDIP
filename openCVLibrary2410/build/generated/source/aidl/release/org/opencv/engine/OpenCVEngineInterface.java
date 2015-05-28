36adaea1e895d96475e8e8f6fc78dd39b2c4db9a
Anton Prokopov <prokopov@hotmail.com>
2015-05-28 22:46:53 +0300

    Info is modified

--- openCVLibrary2410/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary2410/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Nana\\Documents\\GitHub\\MobDIP\\openCVLibrary2410\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /home/anton/MobDIP/MobDIP/openCVLibrary2410/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
